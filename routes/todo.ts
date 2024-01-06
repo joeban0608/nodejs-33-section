83b1cd99f15083c996694f9c68efea81c73cae86
joebanhong <joeanddaneil5@gmail.com>
2024-01-06 17:28:15 +0800

    feat: 33-509 Finishing the REST Routes

--- routes/todo.ts
+++ routes/todo.ts
@@ -3,7 +3,7 @@ import { Todo } from "../models/todo";
 
 const router = Router();
 
-const todoList: Todo[] = [];
+let todoList: Todo[] = [];
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ todoList: todoList });
@@ -15,6 +15,28 @@ router.post("/add-todo", (req, res, next) => {
     text: req.body.text,
   };
   todoList.push(newTodo);
+  return res.status(200).json({ message: "create todo", todoList: todoList });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const tid = req.params.todoId;
+  const todoIndex = todoList.findIndex((todoItem) => todoItem.id === tid);
+  if (todoIndex >= 0) {
+    todoList[todoIndex] = {
+      id: todoList[todoIndex].id,
+      text: req.body.text,
+    };
+    return res
+      .status(200)
+      .json({ message: "Updated todo", todoList: todoList });
+  }
+  return res.status(404).json({ message: "Could not find todo for this id" });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  todoList = todoList.filter((todoItem) => todoItem.id !== req.params.todoId);
+
+  return res.status(200).json({ message: "deleted todo", todoList: todoList });
 });
 
 export default router;
